d70ba61f19ea68cbc76e544637346de9bbabc1e4
Fitsum A <fitsumcs@gmail.com>
2021-04-27 18:25:07 +0400

    Ts data types

--- tu1.ts
+++ tu1.ts
@@ -1,3 +1,49 @@
-let message:String = "Hello World";
+// Data types in typescript [bool, int , string]
 
+// String 
+let message:String = "Hello World";
 console.log(message);
+
+// Boolean 
+let message2:boolean = true;
+console.log(message2);
+
+// Int
+let message3:number = 12;
+console.log(message3);
+
+// Array 
+let list: number[] = [1, 2, 3];
+console.log(list);
+
+// Enum
+enum Color {
+    Red,
+    Green,
+    Blue,
+  }
+  let c: Color = Color.Green;
+
+  console.log(c);
+
+//   Not Sure 
+  let notSure: unknown = 4;
+  console.log(notSure);
+
+//   Any 
+let looselyTyped: any = 4;
+console.log(looselyTyped);
+
+// Void
+let unusable: void = undefined;
+console.log(unusable);
+
+// Not much else we can assign to these variables!
+let u: undefined = undefined;
+let n: null = null;
+console.log(u,n);
+
+
+
+  
+  
